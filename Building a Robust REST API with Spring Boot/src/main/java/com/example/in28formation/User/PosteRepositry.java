bc78dcda490c46dfdbbfe1a9b130958696859a0b
Yousfi Isameddine <76123856+yousfi-issame@users.noreply.github.com>
2023-06-25 22:12:42 +0100

    Delete Building a Robust REST API with Spring Boot directory

--- Building a Robust REST API with Spring Boot/src/main/java/com/example/in28formation/User/PosteRepositry.java
+++
@@ -1,7 +0,0 @@
-package com.example.in28formation.User;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PosteRepositry extends JpaRepository<Post, Integer>{
-
-}
--- Building a Robust REST API with Spring Boot/src/main/java/com/example/in28formation/User/UserRepositry.java
+++
@@ -1,7 +0,0 @@
-package com.example.in28formation.User;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepositry extends JpaRepository<User, Integer>{
-
-}
